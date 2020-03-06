33629a72686abdb1287362260353cc22e3f5ef7e
Romans <50585282+rkononovs@users.noreply.github.com>
2020-03-06 20:49:57 +0000

    Added Script for main menu
    - Added Main menu script
    
    Note - For now I am using string e.g. names of scenes to change them, but after that I may change to index of scene in build.

---
+++ Echolocation/Assets/Scripts/Romans/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private string firstLevel;
+    [SerializeField]
+    private string savedLevel;
+    [SerializeField]
+    private string options;
+    [SerializeField]
+    private string credits;
+    [SerializeField]
+    private string shoeMenu;
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(savedLevel);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void OpenShoeMenu()
+    {
+        SceneManager.LoadScene(shoeMenu);
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene(options);
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene(credits);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
