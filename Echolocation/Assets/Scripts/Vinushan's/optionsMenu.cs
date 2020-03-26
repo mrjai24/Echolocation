372bf5a698d200b1dae7c8aec73a66e149a9603a
Vinushan <vinush@hotmail.co.uk>
2020-03-26 22:39:23 +0000

    Changes to menu system and added death area script

---
+++ Echolocation/Assets/Scripts/Vinushan's/optionsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optionsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newObject = Resources.Load("Prefabs/Options") as GameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
