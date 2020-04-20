83c45ae7ccd2f4d9402e7cad37ac2b001cc5af4e
Romans <50585282+rkononovs@users.noreply.github.com>
2020-04-20 17:47:55 +0300

    Added win and lose screen

--- Echolocation/Assets/Scripts/Romans/Damage.cs
+++ Echolocation/Assets/Scripts/Romans/Damage.cs
@@ -7,8 +7,32 @@ public class Damage : MonoBehaviour
 {
     public AudioClip deathSound;
 
+
+    private static bool isGamePaused = false;
+
+    public GameObject gameStatusUI;
+    public GameObject loseScreen;
+
     [SerializeField]
     private string thisLevel;
+    [SerializeField]
+    private string mainMenu;
+
+    public void Resume()
+    {
+        loseScreen.SetActive(false);
+        gameStatusUI.SetActive(true);
+        Time.timeScale = 1f; // Restart time in game
+        isGamePaused = false;
+    }
+
+    private void Pause()
+    {
+        loseScreen.SetActive(true);
+        gameStatusUI.SetActive(false);
+        Time.timeScale = 0f; // Stop time in game
+        isGamePaused = true;
+    }
 
     private void Start()
     {
@@ -20,7 +44,19 @@ public class Damage : MonoBehaviour
         if (col.gameObject.CompareTag("Player"))
         {
             SoundManager.PlaySound(deathSound);
-            SceneManager.LoadScene(thisLevel);
+            Pause();
         }
+    }
+
+    public void tryAgainButton()
+    {
+        Resume();
+        SceneManager.LoadScene(thisLevel);
+    }
+
+    public void ExitToMainMenu()
+    {
+        Resume();
+        SceneManager.LoadScene(mainMenu);
     }
 }
--- Echolocation/Assets/Scripts/Romans/MoveScenes.cs
+++ Echolocation/Assets/Scripts/Romans/MoveScenes.cs
@@ -5,12 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MoveScenes : MonoBehaviour
 {
+    private static bool isGamePaused = false;
+
+    public GameObject gameStatusUI;
+    public GameObject winScreen;
+
     [SerializeField]
     private string nextLevel;
     [SerializeField] 
     private int bellFlowerNeeded;
+    [SerializeField]
+    private string mainMenu;
 
     public AudioClip WinSound;
+
+    public void Resume()
+    {
+        winScreen.SetActive(false);
+        gameStatusUI.SetActive(true);
+        Time.timeScale = 1f; // Restart time in game
+        isGamePaused = false;
+    }
+
+    private void Pause()
+    {
+        winScreen.SetActive(true);
+        gameStatusUI.SetActive(false);
+        Time.timeScale = 0f; // Stop time in game
+        isGamePaused = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -18,7 +41,19 @@ public class MoveScenes : MonoBehaviour
         if (col.gameObject.CompareTag("Player") && bellFlowers == bellFlowerNeeded) 
         {
             SoundManager.PlaySound(WinSound);
-            SceneManager.LoadScene(nextLevel);
+            Pause();
         }
+    }
+
+    public void nextLevelButton()
+    {
+        Resume();
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    public void ExitToMainMenu()
+    {
+        Resume();
+        SceneManager.LoadScene(mainMenu);
     }
 }
